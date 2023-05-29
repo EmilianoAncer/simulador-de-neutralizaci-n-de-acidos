d868b0cb43d879f93b899ba86b6d4b6ca8279d72
David Sanchez Ortiz <a01283105@itesm.mx>
2023-05-28 23:25:53 -0600

    Bugs Fixed

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,10 +6,20 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     public string sceneName;
+
+    public bool Herramienta1 = false;
+    public bool Herramienta2 = false;
+    public bool Herramienta3 = false;
+    public bool Herramienta4 = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Herramienta1 = false;
+        Herramienta2 = false;
+        Herramienta3 = false;
+        Herramienta4 = false;
     }
 
     // Update is called once per frame
@@ -19,6 +29,30 @@ public class LevelManager : MonoBehaviour
     }
     public void changeScene()
     {
-        SceneManager.LoadScene(sceneName);
+        if(Herramienta1 == true && Herramienta2 == true && Herramienta3 == true && Herramienta4 == true)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        
     }
+
+    public void setTrue(int exa)
+    {
+        if(exa == 1)
+        {
+            Herramienta1 = true;
+        }else if (exa == 2)
+        {
+            Herramienta2 = true;
+        }
+        else if(exa == 3)
+        {
+            Herramienta3 = true;
+
+        }else if (exa == 4)
+        {
+            Herramienta4 = true;
+        }
+    }
+
 }
